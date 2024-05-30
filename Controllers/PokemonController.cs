c3d83818609442045f5cc93c84273b6c813d0a78
Papa_Emmanuelle_BSCS32E1 <ianpapa145@gmail.com>
2024-05-30 15:22:40 +0800

    commit

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using PREFINAL_ASSIGNMENT_TWO_POKEMON_PAPA_EMMANUELLE_BSCS_32E1.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_PAPA_EMMANUELLE_BSCS_32E1.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokemonController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            const int pageSize = 20;
+            var offset = (page - 1) * pageSize;
+            var response = await _httpClient.GetStringAsync($"https://pokeapi.co/api/v2/pokemon?offset={offset}&limit={pageSize}");
+            var data = JObject.Parse(response);
+            var results = data["results"];
+
+            var pokemons = new List<Pokemon>();
+
+            foreach (var result in results)
+            {
+                pokemons.Add(new Pokemon { Name = result["name"].ToString() });
+            }
+
+            ViewBag.Page = page;
+            ViewBag.HasPrevious = offset > 0;
+            ViewBag.HasNext = data["next"] != null;
+
+            return View(pokemons);
+        }
+
+        public async Task<IActionResult> Details(string name)
+        {
+            var response = await _httpClient.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{name}");
+            var data = JObject.Parse(response);
+
+            var pokemon = new Pokemon
+            {
+                Name = data["name"].ToString(),
+                Moves = data["moves"].Select(m => m["move"]["name"].ToString()).ToList(),
+                Abilities = data["abilities"].Select(a => a["ability"]["name"].ToString()).ToList()
+            };
+
+            return View(pokemon);
+        }
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_PAPA_EMMANUELLE_BSCS_32E1.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Moves { get; set; }
+        public List<string> Abilities { get; set; }
+    }
+}
